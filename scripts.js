6e28c6a04c279187660df44f94469b02b0da3d28
adamhlomgern <146996877+adamhlomgern@users.noreply.github.com>
2024-11-13 18:37:25 +0100

    ad

--- scripts.js
+++ scripts.js
@@ -11,6 +11,3 @@ function ShowHide(element) {
     // Toggle hamburger icon animation (change to X)
     element.classList.toggle("change");
 }
-
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=ID"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'ID');</script>
